575692a7ba9474108d35eb8238157d21e07c4414
Jason Boudreaux <js.bdrx@gmail.com>
2022-03-05 19:05:32 -0600

    started index.js for Footer, added placeholders

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            {/* <div>
+                add github logo/link here
+            </div> */}
+            {/* <div>
+                add linked in logo/link here
+            </div> */}
+        </footer>
+    )
+}
+
+export default Footer;
