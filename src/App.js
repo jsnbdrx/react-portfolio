6450545b9233018fd37fbbe2dec44e0dba3daa6b
Jason Boudreaux <js.bdrx@gmail.com>
2022-03-05 19:00:59 -0600

    set up App.js with all files that will be used/created, created index.js for Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   return (
